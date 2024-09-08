79367c94f8fa23d4f2ff41750b4035e7200df900
Toksy <73304133+Shailesh744@users.noreply.github.com>
2024-09-08 06:34:33 +0000

    this is initial comment

---
+++ quest1.py
@@ -0,0 +1,17 @@
+# Write a Python code to declare different types of variable. 
+print("here is the all listed variable in python")
+
+a=10
+print(a) #this is integer
+b=1.2
+print(b) #this is float value
+
+c="shailesh"
+print(f"this is string value :{c}")
+d=0 
+print(f"this is assignment operator{d}")
+j=complex(5,6)
+print(j)
+f=3+5J
+print(f)
+
